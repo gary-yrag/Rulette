139b2097c393f78efae5b53a49a6c6f46c9f9a91
Gary J <gary-yrag@hotmail.com>
2021-02-17 00:35:40 -0500

    Creation package entitys

---
+++ src/main/java/com/init/rulette/entitys/rulette.java
@@ -0,0 +1,19 @@
+package com.init.rulette.entitys;
+
+public class rulette {
+	private int id;
+	private String status;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
