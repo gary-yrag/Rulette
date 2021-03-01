50b2f74efd9ce302c61fd847e93a9ed559228e3d
Gary J <gary-yrag@hotmail.com>
2021-03-01 00:11:37 -0500

    Add methods in controller rutting for entity rulette

--- src/main/java/com/init/rulette/rest/RoutingRest.java
+++ src/main/java/com/init/rulette/rest/RoutingRest.java
@@ -1,10 +1,15 @@
 package com.init.rulette.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,20 +26,43 @@ public class RoutingRest {
 	
 	@Autowired
 	private RuletteDao ruletteDao; 
-	
-	//@GetMapping
-	@RequestMapping(value="getRulettes", method= RequestMethod.GET)
+		
+	//@RequestMapping(value="getRulettes", method= RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<rulette>> getRulettes(){
 		List<rulette> rulettes =  ruletteDao.findAll();
 		return ResponseEntity.ok(rulettes);
 	}
 	
+	@RequestMapping(value="{ruletteId}", method= RequestMethod.GET)
+	public ResponseEntity <rulette>getRuletteById(@PathVariable("ruletteId") Long productId) {
+		Optional<rulette> optionalRulette = ruletteDao.findById(productId);
+		
+		if(optionalRulette.isPresent()) {
+			return ResponseEntity.ok(optionalRulette.get());
+		}else {
+			return ResponseEntity.noContent().build();
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity <rulette>CreateRulette(@RequestBody rulette Rulette){
+		rulette newRulette = ruletteDao.save(Rulette);
+		return ResponseEntity.ok(newRulette);
+	}
+	
+	@DeleteMapping(value="{productId}")
+	public ResponseEntity <Void> deleteRulette(@PathVariable("productId")Long produdctId){
+		ruletteDao.deleteById(produdctId);
+		return ResponseEntity.ok(null);
+	}
 	
 	//@GetMapping //Disponble Mediante metodo get //no define una Url (Toma como url la base /) localhost:8080
-	@RequestMapping(value="hello", method = RequestMethod.GET)
+	/*@RequestMapping(value="hello", method = RequestMethod.GET)
 	public String hello() {
 		return "Hellow Word";
 	}
+	*/
 	
 	/*
 	@GetMapping
@@ -47,10 +75,12 @@ public class RoutingRest {
 	}
 	*/
 	
+	/*
 	@RequestMapping(value="getRulette", method = RequestMethod.GET)
 	public ResponseEntity<List<com.init.rulette.entitysJPA.rulette>> getsRulette(){
 		return null;
 	}
+	*/
 	
 	//public ResponseEntity<List<rulette>> getRulettes(){
 		
